a33144272eef14465f0fb19310b3033b709123ca
zhaoyifan <993445491@qq.com>
2021-12-23 23:01:46 +0800

    feat: 主页和留言板

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import { BrowserRouter as Router, Link,NavLink , Route, Routes, Navigate } from "react-router-dom";
 import { useState } from "react";
 import "./App.css";
+import Home from './home'
+import MessageBoard from './messageBoard'
 
-function App() {
+function App () {
   const [message, setMessage] = useState("Ready...");
 
   const onDemoClick = () => {
@@ -16,9 +19,18 @@ function App() {
 
   return (
     <div className="App">
-      <button onClick={onDemoClick}>Demo Button</button>
-      <p>{message}</p>
+      <Router>
+        <NavLink to="/home" className="link">主页</NavLink>
+        <NavLink to="/messageBoard" className="link">留言板</NavLink>
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/home" element={<Home/>}></Route>
+          <Route path="/messageBoard" element={<MessageBoard/>}></Route>
+        </Routes>
+        <Navigate to="/home" element={<Home/>}/>    {/* 当以上Route组件都匹配失败时，重定向到/home */}
+      </Router>
     </div>
+
   );
 }
 
---
+++ src/home/index.js
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App () {
+
+  return (
+    <div className="App">
+      主页
+    </div>
+
+  );
+}
+
+export default App;
